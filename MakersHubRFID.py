2a38ef9c8caed75ab62d3a9fcea06325ce5b274b
Mathew Norman <spam@matnorman.com>
2018-04-10 09:09:29 +1000

    Small updates

---
+++ MakersHubRFID.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Import libraries
+from gpiozero import Buzzer # Import library for buzzer
+from time import sleep # Import library for sleep
+import RPi.GPIO as GPIO # Import library for GPIO pins
+import SimpleMFRC522 as RC522 # Import library for RFID Reader
+
