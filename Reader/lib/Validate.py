cf83ba8c974049b7eb7e28e7a1bfdc7ad6c0e676
Mathew Norman <spam@matnorman.com>
2018-04-14 07:54:47 +1000

    Fixing unusual error

--- Reader/lib/Validate.py
+++ Reader/lib/Validate.py
@@ -8,7 +8,7 @@
 class Validate:
 
     # Example list of valid card IDs
-    validCards = [17988527649, 595116637326, 252747632322]
+	validCards = [17988527649, 595116637326, 252747632322]
 
 	def card(self, id):
 		if id in validCards:
