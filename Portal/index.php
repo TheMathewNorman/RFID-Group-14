3f5103da5458b60ad6928206c8acdf1b58cf69ad
Mathew Norman <spam@matnorman.com>
2018-04-25 18:49:23 +1000

    Rewriting index and creating global styling

--- Portal/index.php
+++ Portal/index.php
@@ -1,33 +1,20 @@
 <!DOCTYPE html>
-<html lang="en" >
-<!--Created by Team 14 Deakin University Students-->
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>Makers Hub RFID Tag System</title>
-      <link rel="stylesheet" href="css/style.css">
+<meta charset="UTF-8" />
+<title>Access Control Management System</title>
 </head>
-
 <body>
-<h1>We've got a domain!</h1>
-  <div class="login-page">
-  <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
-    <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
-    </form>
-  </div>
-</div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-    <script  src="js/index.js"></script>
-</body>
+    <div id="login-container">
 
+        <div id="login-form">
+            <form>
+                <input type="text" name="username" placeholder="Username"><br>
+                <input type="password" name="password" placeholder="Password"><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+
+    </div>
+</body>
 </html>
