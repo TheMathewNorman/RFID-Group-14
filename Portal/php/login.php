92089f9d7d87b88ad561b9fed185fd2b05577bf3
azdeveloping <adayanan@deakin.edu.au>
2018-05-12 09:18:41 +1000

    Add files via upload

--- Portal/php/login.php
+++ Portal/php/login.php
@@ -11,24 +11,24 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "");
+$connection = mysqli_connect("localhost", "root", "");
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
+$username = mysqli_real_escape_string($username);
+$password = mysqli_real_escape_string($password);
 // Selecting Database
-$db = mysql_select_db("therfidmen", $connection);
+$db = mysqli_select_db("therfidmen", $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
-$rows = mysql_num_rows($query);
+$query = mysqli_query("select * from login where password='$password' AND username='$username'", $connection);
+$rows = mysqli_num_rows($query);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session
 header("location: profile.php"); // Redirecting To Other Page
 } else {
 $error = "Username or Password is invalid";
 }
-mysql_close($connection); // Closing Connection
+mysqli_close($connection); // Closing Connection
 }
 }
 ?>
--- Portal/php/session.php
+++ Portal/php/session.php
@@ -1,18 +1,17 @@
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "");
+$connection = mysqli_connect("localhost", "root", "");
 // Selecting Database
-$db = mysql_select_db("therfidmen", $connection);
+$db = mysqli_select_db("therfidmen", $connection);
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
-$row = mysql_fetch_assoc($ses_sql);
+$ses_sql=mysqli_query("select username from login where username='$user_check'", $connection);
+$row = mysqli_fetch_assoc($ses_sql);
 $login_session =$row['username'];
 if(!isset($login_session)){
-mysql_close($connection); // Closing Connection
+mysqli_close($connection); // Closing Connection
 header('Location: index.php'); // Redirecting To Home Page
 }
-// Test change
 ?>
