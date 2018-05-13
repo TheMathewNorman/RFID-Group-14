77ccb857bd9a2b8276333ec6b003d621b722b5b4
Mathew Norman <spam@matnorman.com>
2018-05-14 07:56:31 +1000

    Updated db test form

--- Portal/php/dbtest.php
+++ Portal/php/dbtest.php
@@ -5,14 +5,20 @@
     $database = new Database;
     $database->createTables();
 
+    echo "Tables created successfully.\n";
+
     // Insert test member
     $database->addMember("Mathew Norman", "someone@example.com", "0400000000", "0192889112412345643");
+    echo "Member add test 1.\n";
     $database->addMember("Mathew Norman", "someoneelse@example.com", "0400000000", "asq2889112412345643");
+    echo "Member add test 2.\n";
 
     // Update test member
     $database->updateMember('1', "Mathew Norman", "coolemail@mat.com", "", "");
+    echo "Member update test.\n";
 
     // Delete test member
     $database->deleteMember('2');
+    echo "Member delete test.\n";
 
 ?>
