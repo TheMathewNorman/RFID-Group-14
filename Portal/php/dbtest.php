65898fdf6fa86b8e66cd11cb935caa7ec66a14ee
Mathew Norman <spam@matnorman.com>
2018-05-14 06:50:39 +1000

    More DB tests

--- Portal/php/dbtest.php
+++ Portal/php/dbtest.php
@@ -1,17 +1,15 @@
 <?php    
     include "sqlcreds.php";
 
-    echo $GLOBALS['server'] . "A";
-    echo $GLOBALS['user'] . "A";
-    echo $GLOBALS['pass'] . "A";
-    echo $GLOBALS['rfidDB'] . "A";
     // Create connection
     $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['rfidDB']);
     
     // Check connection
     if ($connection->connect_error) {
-        //die("Connection failed: " . $connection->connect_error);
-    } 
+        echo "Connection failed: " . $connection->connect_error;
+    } else {
+        echo "Connection success\n";
+    }
 
     // Create the members table upon success
     $sql = "CREATE TABLE staff (
@@ -22,7 +20,9 @@
     )";
         
     if ($connection>query($sql) !== TRUE) {
-        //die("Error creating table: " . $connection->error);
+        echo "Error creating table: " . $connection->error;
+    } else {
+        echo "Creation success";
     }
 
     $connection->close();
