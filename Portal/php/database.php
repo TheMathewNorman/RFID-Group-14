411d3277d0331b4b8d68e5e2a089969c04c26136
Mathew Norman <spam@matnorman.com>
2018-05-14 08:15:29 +1000

    Added a listMembers function

--- Portal/php/database.php
+++ Portal/php/database.php
@@ -53,7 +53,37 @@ class Database {
     }
 
     function listMembers() {
+        // Create connection
+        $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
+                
+        // Check connection
+        if ($connection->connect_error) {
+            die("Connection failed<br>$connection->connect_error");
+        }
 
+        // Form SQL query
+        $sql = "SELECT id, fullname, email, phone FROM members ORDER BY id";
+
+        // Fetch each line and display in table
+        if ($result = mysqli_query($connection, $sql)) {
+            
+            ////// TEST OUTPUT FROM NUM ROWS
+            echo mysqli_num_rows($result);
+            ///// REMOVE AFTER TEST
+
+            if (mysqli_num_rows($result) === 0) {
+                echo "The members table is empty.";
+            } else {
+                echo '<table><tr><th></th><th></th><th></th><th></th></tr>';
+                while ($row=mysqli_fetch_row($result)) {
+                    echo "<tr><td>".$row['id']."</td><td>".$row['fullname']."</td><td>".$row['email']."</td><td>".$row['phone']."</td></tr>";
+                }
+                echo '</table>';
+            }
+            mysqli_free_result($result);
+        }
+
+        $connection->close();
     }
 
     function addMember($fullname, $email, $phone, $cardkey) {
@@ -126,6 +156,8 @@ class Database {
         if ($connection->query($sql) !== TRUE) {
             die("Error deleting member<br>$connection->error");
         }
+
+        $connection->close();
     }
 
 
--- Portal/php/dbtest.php
+++ Portal/php/dbtest.php
@@ -7,18 +7,30 @@
 
     echo "Tables created successfully.<br>";
 
+    // List members
+    $database->listMembers();
+
     // Insert test member
     $database->addMember("Mathew Norman", "someone@example.com", "0400000000", "0192889112412345643");
     echo "Member add test 1.<br>";
     $database->addMember("Mathew Norman", "someoneelse@example.com", "0400000000", "asq2889112412345643");
     echo "Member add test 2.<br>";
 
+    // List members
+    $database->listMembers();
+
     // Update test member
     $database->updateMember('1', "Mathew Norman", "coolemail@mat.com", "", "");
     echo "Member update test.<br>";
 
+    // List members
+    $database->listMembers();
+
     // Delete test member
     $database->deleteMember('2');
     echo "Member delete test.<br>";
+
+    // List members
+    $database->listMembers();
 
 ?>
