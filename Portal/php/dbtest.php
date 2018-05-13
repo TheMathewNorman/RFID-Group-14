19a428aa56e2870f74afc97ccdf08708fa697ca8
Mathew Norman <spam@matnorman.com>
2018-05-14 09:22:04 +1000

    More DB tests

--- Portal/php/dbtest.php
+++ Portal/php/dbtest.php
@@ -8,35 +8,37 @@
     echo "Tables created successfully.<br>";
 
     // List members
-    $database->listMembers();
     echo "Member list test.<br>";
+    $database->listMembers();
 
     // Insert test member
-    $database->addMember("Mathew Norman", "someone@example.com", "0400000000", "0192889112412345643");
     echo "Member add test 1.<br>";
-    $database->addMember("Mathew Norman", "someoneelse@example.com", "0400000000", "asq2889112412345643");
+    $database->addMember("Mathew Norman", "someone@example.com", "0400000000", "0192889112412345643");
     echo "Member add test 2.<br>";
+    $database->addMember("Mathew Norman", "someoneelse@example.com", "0400000000", "asq2889112412345643");
 
+    echo "Search test 1.<br>";
     $database->searchMembers("Ma");
+    echo "Search test 2.<br>";
     $database->searchMembers("abcd");
 
     // List members
-    $database->listMembers();
     echo "Member list test.<br>";
+    $database->listMembers();
 
     // Update test member
-    $database->updateMember('1', "Mathew Norman", "coolemail@mat.com", "", "");
     echo "Member update test.<br>";
+    $database->updateMember('1', "Mathew Norman", "coolemail@mat.com", "", "");
 
     // List members
-    $database->listMembers();
     echo "Member list test.<br>";
+    $database->listMembers();
 
     // Delete test member
-    $database->deleteMember('2');
     echo "Member delete test.<br>";
+    $database->deleteMember('2');
 
     // List members
-    $database->listMembers();
     echo "Member list test.<br>";
+    $database->listMembers();
 ?>
