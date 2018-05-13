c980ee7c484cc2fbbc6b8e5ed5a947baa5df7275
Mathew Norman <spam@matnorman.com>
2018-05-14 07:19:53 +1000

    More DB tests
    Testing the database class and create tables method.

--- Portal/php/database.php
+++ Portal/php/database.php
@@ -9,50 +9,49 @@ class Database {
     //private $password = "";
 
     // Create the database and table
-    function create() {
+    function createtables() {
 
         // Create connection
-        $connection = new mysqli($server, $username, $password);
+        $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
         
         // Check connection
         if ($connection->connect_error) {
-            die("Connection failed: " . $connection>connect_error);
-        } 
+            die("Connection failed: " . $connection->connect_error);
+        }
 
-        // Create database
-        $sql = "CREATE DATABASE rfidDatabase";
-        if ($connection->query($sql) === TRUE) {
-            echo "Database created successfully";
-
-            // Create the members table upon success
-            $sql = "CREATE TABLE staff (
-                ///////////////////////////////
-                // ADD SQL FOR TABLE HERE //
-                ///////////////////////////////
-            );CREATE TABLE members(
-                ///////////////////////////////
-                // ADD SQL FOR TABLE HERE //
-                ///////////////////////////////
-            );CREATE TABLE priviledge (
-                ///////////////////////////////
-                // ADD SQL FOR TABLE HERE //
-                ///////////////////////////////
-            );CREATE TABLE logs(
-                ///////////////////////////////
-                // ADD SQL FOR TABLE HERE //
-                ///////////////////////////////
-            );CREATE TABLE rfid_unit(
-                ///////////////////////////////
-                // ADD SQL FOR TABLE HERE //
-                ///////////////////////////////
-            )";
+        // Create the members table upon success
+        $sql = "CREATE TABLE admin (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            fullname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            phone VARCHAR(10),
+            passhash VARCHAR(128) NOT NULL
+        ); CREATE TABLE member (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            fullname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            phone VARCHAR(10),
+            cardkey VARCHAR(128) NOT NULL
+        ); CREATE TABLE priviledge (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            member_id INT(6) NOT NULL,
+            reader_id INT(6),
+            reader_group INT(6)
+        ); CREATE TABLE reader (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            reader_name VARCHAR(30) NOT NULL,
+            reader_group INT(6) NOT NULL
+        ); CREATE TABLE logs (
+            id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            member_id INT(6) NOT NULL,
+            reader_id INT(6) NOT NULL,
+            access_date TIMESTAMP NOT NULL
+        )";
             
-            if ($connection->query($sql) !== TRUE) {
-                echo "Error creating table: " . $connection->error;
-            }
-
+        if (mysqli_multi_query($connection, $sql) === FASLE) {
+            die("Error creating table: " . $connection->error);
         } else {
-            echo "Error creating database: " . $connection->error;
+            echo "Creation success";
         }
 
         $connection->close();
--- Portal/php/dbtest.php
+++ Portal/php/dbtest.php
@@ -1,48 +1,7 @@
 <?php    
-    include "sqlcreds.php";
+    include "database.php";
 
-    // Create connection
-    $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
-    
-    // Check connection
-    if ($connection->connect_error) {
-        die("Connection failed: " . $connection->connect_error);
-    }
+    $database = new Database;
+    $database->createtables();
 
-    // Create the members table upon success
-    $sql = "CREATE TABLE admin (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        fullname VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        phone VARCHAR(10),
-        passhash VARCHAR(128) NOT NULL
-    ); CREATE TABLE member (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        fullname VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        phone VARCHAR(10),
-        cardkey VARCHAR(128) NOT NULL
-    ); CREATE TABLE priviledge (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        member_id INT(6) NOT NULL,
-        reader_id INT(6),
-        reader_group INT(6)
-    ); CREATE TABLE reader (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        reader_name VARCHAR(30) NOT NULL,
-        reader_group INT(6) NOT NULL
-    ); CREATE TABLE logs (
-        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        member_id INT(6) NOT NULL,
-        reader_id INT(6) NOT NULL,
-        access_date TIMESTAMP NOT NULL
-    )";
-        
-    if (mysqli_multi_query($connection, $sql) === FASLE) {
-        die("Error creating table: " . $connection->error);
-    } else {
-        echo "Creation success";
-    }
-
-    $connection->close();
 ?>
