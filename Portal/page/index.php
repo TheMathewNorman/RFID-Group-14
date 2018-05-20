693e2de97cbeb4a4b61f6fe41eab6638ba74d628
Mathew Norman <spam@matnorman.com>
2018-05-21 06:47:47 +1000

    Fixed gitignore error
    - Gitignore was ignoring ALL index.php files.

---
+++ Portal/page/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../css/global-style.css">
+    <link rel="stylesheet" type="text/css" href="../css/menu.css">
+</head>
+<body>
+    <div id="content">
+    <?php include "menu.php"; ?>
+    This is a temporary home page.
+</div>
+</body>
+</html>
