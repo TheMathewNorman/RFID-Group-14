ee7a0d3bce01c4a5040daa5bcbebbb535e1b1bb4
Mathew Norman <spam@matnorman.com>
2018-05-22 10:21:13 +1000

    Testing delete user function.

---
+++ Portal/php/deleteuser.php
@@ -0,0 +1,20 @@
+<?php
+    include_once "database.php";
+    $database = new Database();
+
+    $table = $_GET['table'];
+    $userid = $_GET['id'];
+
+    echo "$table<br>$userid";
+
+    // $redirect = "../page/index.php";
+
+    // if ($table == "admin") {
+    //     $database->removeAdmin($userid);
+    //     $redirect = "../page/listadmin.php";
+    // } else if ($table == "member") {
+    //     $database->removeMember($userid);
+    //     $redirect = "../page/listmember.php";
+    // }
+    // header("Location: $redirect");
+?>
