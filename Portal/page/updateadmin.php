e4d8ac84ab4f3bcd9840f80619ec0778118cd9ea
Mathew Norman <spam@matnorman.com>
2018-05-22 14:07:31 +1000

    Minor fixes
    Solved issue; PEBKAC

--- Portal/page/updateadmin.php
+++ Portal/page/updateadmin.php
@@ -20,15 +20,13 @@
         
         <?php include "../component/header.php"; ?>
         <?php include "../component/menu.php"; ?>
-            
-            <?php var_dump($userInfo); ?>
 
             <form action="" method="POST">
                 <table class="form-table">
-                <tr><td style="text-align:right">First name: </td><td><input type="text" name="firstname" placeholder="<?php $userInfo['fname']; ?>"></td></tr>
-                <tr><td style="text-align:right">Last name: </td><td><input type="text" name="lastname" placeholder="<?php $userInfo['fname']; ?>"></td></tr>
-                <tr><td style="text-align:right">Email: </td><td><input type="email" name="email"  placeholder="<?php $userInfo['fname']; ?>"></td></tr>
-                <tr><td style="text-align:right">Phone: </td><td><input type="text" name="phone" placeholder="<?php $userInfo['fname']; ?>"></td></tr>
+                <tr><td style="text-align:right">First name: </td><td><input type="text" name="firstname" placeholder="<?php echo $userInfo['fname']; ?>"></td></tr>
+                <tr><td style="text-align:right">Last name: </td><td><input type="text" name="lastname" placeholder="<?php echo $userInfo['lname']; ?>"></td></tr>
+                <tr><td style="text-align:right">Email: </td><td><input type="email" name="email"  placeholder="<?php echo $userInfo['email']; ?>"></td></tr>
+                <tr><td style="text-align:right">Phone: </td><td><input type="text" name="phone" placeholder="<?php echo $userInfo['phone']; ?>"></td></tr>
                 <tr><td style="text-align:right">Password: </td><td><input type="password" name="password"></td></tr>
                 <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="Delete"></td></tr>
                 <tr><td colspan="2" style="text-align:right"><input type="submit" value="Update Member"> <input type="reset" value="Clear"></td></tr>
