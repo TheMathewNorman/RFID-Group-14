85c410d4e31362867023b1c163d7e07e90f6a5b9
Mathew Norman <spam@matnorman.com>
2018-05-22 10:23:16 +1000

    Adjusted first run layout

--- Portal/component/firstrun.php
+++ Portal/component/firstrun.php
@@ -43,9 +43,10 @@
     <div id="content">
             <h1>First-run Setup</h1>
             <p>This system requires some information before it can be used.<br>All of the following required before use. This information can be changed later if necessary.</p>
-            <table>
+            
+            Database:
             <form action="" method="post">
-                <tr><td colspan="2" style="font-weight:bolder;padding-bottom:5px;">Database:</td></tr>
+            <table>
                 <tr><td style="text-align:right">Server: </td><td><input type="text" name="server" value="localhost" required></td></tr>
                 <tr><td style="text-align:right">Database: </td><td><input type="text" name="dbname" required></td></tr>
                 <tr><td style="text-align:right">Username: </td><td><input type="text" name="dbuser" required></td></tr>
@@ -59,8 +60,8 @@
                 <tr><td style="text-align:right">Phone: </td><td><input type="text" name="phone" required></td></tr>
                 <tr><td style="text-align:right">Password: </td><td><input type="password" name="password" required></td></tr>
                 <tr><td colspan="2" style="text-align:right"><input type="submit" value="Run Setup"> <input type="reset" value="Clear"></td></tr>
-            </form>
             </table>
+            </form>
     </div>
 </body>
 </html>
