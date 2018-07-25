9df9f18f1d417e87666d756f0ad913d6da84cddb
Mathew Norman <spam@matnorman.com>
2018-07-25 17:55:09 +1000

    Created sessions template

---
+++ Portal/php/sessions.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+class Sessions {
+
+    // Create a session
+    function startSession() {
+
+    }
+
+    // End a Session
+    function endSession() {
+
+    }
+
+}
+
+?>
