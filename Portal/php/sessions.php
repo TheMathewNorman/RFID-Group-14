2725b5bb55ab485e281ec98feccc9ce46efa1409
azdeveloping <adayanan@deakin.edu.au>
2018-07-25 18:10:14 +1000

    Update sessions.php

--- Portal/php/sessions.php
+++ Portal/php/sessions.php
@@ -16,3 +16,21 @@ class Sessions {
 }
 
 ?>
+
+<?php
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+    $connection = mysqli_connect("localhost", "root", "");
+// Selecting Database
+    $db = mysqli_select_db("therfidmen", $connection);
+    session_start();// Starting Session
+// Storing Session
+    $user_check=$_SESSION['login_user'];
+// SQL Query To Fetch Complete Information Of User
+    $ses_sql=mysqli_query("select username from login where username='$user_check'", $connection);
+    $row = mysqli_fetch_assoc($ses_sql);
+    $login_session =$row['username'];
+    if(!isset($login_session)){
+    mysqli_close($connection); // Closing Connection
+    header('Location: index.php'); // Redirecting To Home Page
+}
+?>
