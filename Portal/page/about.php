02de8c8693702a2315fcaeda39a3b21be972c999
Mathew Norman <spam@matnorman.com>
2018-08-02 19:27:45 +1000

    About page template

---
+++ Portal/page/about.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../css/global-style.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/menu.css">
+</head>
+<body>
+    <div id="content">
+    <?php include "../component/header.php"; ?>
+    <?php include "../component/menu.php"; ?>
+    
+    <!-- Write about page content here -->
+
+
+</div>
+</body>
+</html>
