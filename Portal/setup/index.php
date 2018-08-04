5e4e4dc4c66e3334640f230f363511263a0c777d
Mathew Norman <spam@matnorman.com>
2018-08-04 19:28:30 +1000

    Setup form size animation

--- Portal/setup/index.php
+++ Portal/setup/index.php
@@ -18,14 +18,45 @@
         <div id="setup-text">
             Some information is required before being able to use this system.
         </div>
-        <?php
-            if ($_GET['page'] == '2') {
-                include "newadmin.php";
-            } else {
-                include "database.php";
-            }
-        ?>
+        
+        <!-- Database setup form -->
+        <div id="setup-form">
+        <form action="" method="POST">
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                <div class="input-box"><input type="text" name="server" placeholder="Server"></div>
+            </div>
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                <div class="input-box"><input type="text" name="dbuser" placeholder="Username"></div>
+            </div>
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-lock fa-lg"></i></div>
+                <div class="input-box"><input type="password" name="dbpass" placeholder="Password"></div>
+            </div>
+            <input type="submit" value="Submit">
+            </form>
+        </div>
 
+    <!-- Admin setup form 
+    <div id="setup-form">
+        <form action="index.php?page=2" method="POST">
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                <div class="input-box"><input type="text" name="fname" placeholder="Server"></div>
+            </div>
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                <div class="input-box"><input type="text" name="lname" placeholder="Username"></div>
+            </div>
+            <div class="form-field">
+                <div class="input-icon"><i class="fas fa-lock fa-lg"></i></div>
+                <div class="input-box"><input type="password" name="email" placeholder="Password"></div>
+            </div>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+-->
     </div>
 </body>
 </html>
