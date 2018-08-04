b0275caab473f9c1742f7caaf653dec21ac3f21e
Mathew Norman <spam@matnorman.com>
2018-08-04 18:30:37 +1000

    file write test

--- Portal/php/testxml.php
+++ Portal/php/testxml.php
@@ -1,4 +1,4 @@
 <?php
-
+    $file = 'file.txt';
+    $handle = fopen($file, 'w') or die('Cannot open file:  '.$file); //implicitly creates file
 ?>
-<a href="http://therfid.men/php/sqlcreds.php">http://therfid.men/php/sqlcreds.php</a>
