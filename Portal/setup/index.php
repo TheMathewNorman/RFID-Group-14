6bbcc74cf250267da373facaa836963c0570d6e6
Mathew Norman <spam@matnorman.com>
2018-08-04 18:42:27 +1000

    Creating setup procedure

---
+++ Portal/setup/index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Access Control Management System</title>
+
+<link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div id="setup-container">
+        <div id="setup-heading">
+            First Run Setup
+        </div>
+        <div id="setup-form">
+            <form action="" method="POST">
+                <div class="form-field">
+                    <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                    <div class="input-box"><input type="text" name="server" placeholder="Server"></div>
+                </div>
+                <div class="form-field">
+                    <div class="input-icon"><i class="fas fa-user fa-lg"></i></div>
+                    <div class="input-box"><input type="text" name="dbuser" placeholder="Username"></div>
+                </div>
+                <div class="form-field">
+                    <div class="input-icon"><i class="fas fa-lock fa-lg"></i></div>
+                    <div class="input-box"><input type="password" name="dbpass" placeholder="Password"></div>
+                </div>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+
+    </div>
+</body>
+</html>
