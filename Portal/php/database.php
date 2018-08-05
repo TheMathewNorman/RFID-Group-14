19c0a58f453fd5e135afa4ec17791a6ada45b99c
Mathew Norman <spam@matnorman.com>
2018-08-05 18:41:21 +1000

    Debugging login

--- Portal/php/database.php
+++ Portal/php/database.php
@@ -165,33 +165,33 @@ class Database {
         include_once "./php/sessions.php";
         $sessions = new Sessions();
 
-        return "Sessions class has been included";
+        // return "Sessions class has been included";
 
-        // // Create connection
-        // $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
+        // Create connection
+        $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
                 
-        // // Check connection
-        // if ($connection->connect_error) {
-        //     die("Connection failed<br>$connection->connect_error");
-        // }
+        // Check connection
+        if ($connection->connect_error) {
+            die("Connection failed<br>$connection->connect_error");
+        }
 
-        // $sql = "SELECT id, firstname FROM admins WHERE email LIKE $email AND passhash LIKE $passhash";
+        $sql = "SELECT id, firstname FROM admins WHERE email LIKE $email AND passhash LIKE $passhash";
 
-        // // If there are no users matching the email/passhash in the admins db, return false otherwise create session & return true.
-        // if ($result = mysqli_query($connection, $sql)) {
-        //     if (mysqli_num_rows($result) > 0) {
-        //         // Get user details
-        //         $row = mysqli_fetch_row($result);
-        //         // Create session
-        //         $sessions->startSession($row[0]. $row[1]);
-        //         // Close mysqli connection
-        //         $connection->close();
-        //         return true;
-        //     } else {
-        //         $connection->close();
-        //         return false;
-        //     }
-        // }
+        // If there are no users matching the email/passhash in the admins db, return false otherwise create session & return true.
+        if ($result = mysqli_query($connection, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                // Get user details
+                $row = mysqli_fetch_row($result);
+                // Create session
+                $sessions->startSession($row[0]. $row[1]);
+                // Close mysqli connection
+                $connection->close();
+                return true;
+            } else {
+                $connection->close();
+                return false;
+            }
+        }
 
     }
     
