82c648da0cfa93db3c5f0656603f95c37ed099d5
Mathew Norman <spam@matnorman.com>
2018-08-09 22:56:28 +1000

    Disabled validation for demonstration

--- Portal/js/input.js
+++ Portal/js/input.js
@@ -1,14 +1,14 @@
 function sanitizeInput(str){
-    sanInput = str.trim(str.toLowerCase());
-    // Not sure if assigning it back to the variable is necessary?
-    sanInput = sanInput.replace(/<[^>]*>/g, '');
-    return sanInput;
+    // sanInput = str.trim(str.toLowerCase());
+    // // Not sure if assigning it back to the variable is necessary?
+    // sanInput = sanInput.replace(/<[^>]*>/g, '');
+    // return sanInput;
 }
 
 function validateInput() {
-    var input = sanitizeInput(document.forms["search"]["searchInput"].value);
-    if(input != /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@*((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-        input = "";
-    if(input != "")
-        document.forms["search"]["searchInput"].value = input
+    // var input = sanitizeInput(document.forms["search"]["searchInput"].value);
+    // if(input != /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@*((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    //     input = "";
+    // if(input != "")
+    //     document.forms["search"]["searchInput"].value = input
 }
