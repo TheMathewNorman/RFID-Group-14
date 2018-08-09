d5a607db59c311178a8e051f9df50af412a56242
Mathew Norman <spam@matnorman.com>
2018-08-09 23:39:00 +1000

    Test reponse codes

--- Portal/php/response.test.php
+++ Portal/php/response.test.php
@@ -1,11 +1,13 @@
 <?php    
     if (isset($_GET['test'])) {
         if ($_GET['test'] == "good") {
-            // 200 OK
-            http_response_code(200);
+            // 401 Unauthorized
+            http_response_code(401);
+            echo "Unauthorized";
         } else {
             // 401 Unauthorized
             http_response_code(401);
+            echo "Unauthorized";
         }
     }
 ?>
