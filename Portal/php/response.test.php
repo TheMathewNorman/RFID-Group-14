959c17983cdd2610558004f368edc18f0f808128
Mathew Norman <spam@matnorman.com>
2018-08-09 23:46:25 +1000

    testing reponse codes

--- Portal/php/response.test.php
+++ Portal/php/response.test.php
@@ -1,9 +1,13 @@
 <?php    
     if (isset($_GET['test'])) {
         if ($_GET['test'] == "good") {
-            echo true;
-        } else {
-            echo false;
+            // 200 OK
+            http_response_code(200);
+            echo "OK";
         }
+    } else {
+        // 403 Forbidden
+        http_response_code(403);
+        echo "Forbidden";
     }
 ?>
