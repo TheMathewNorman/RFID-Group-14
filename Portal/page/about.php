42f7e00086e460a29af0c6dc3d1fbfc57084cb21
Mathew Norman <spam@matnorman.com>
2018-08-10 02:48:41 +1000

    Formatting about content

--- Portal/page/about.php
+++ Portal/page/about.php
@@ -10,25 +10,26 @@
     <?php include "../component/header.php"; ?>
     <?php include "../component/menu.php"; ?>
     
-    <p>This system is underdevelopment by Group 14 from Deakin University.</p>
-    
-    <p>The purpose of this system is to securely manage user access to both areas and equipment.</p>
+    <div style="padding:10px">
+        <p>This system is under development by Group 14 from Deakin University.</p>
+        
+        <p>The purpose of this system is to securely manage user access to both areas and equipment.</p>
 
-    <p>
-    Development credits: 
-        <ul>
-        <li>Adam Smeaton</li>
-        <li>Asrin Dayananda</li>
-        <li>Mathew Norman</li>
-        <li>Michael Ferris</li>
-        <li>Yuan Ren</li>
-        <li>Jonathon Davis</li>
-        </ul>
-    </p>
-    
-    <p>If you experience any problems with this system, please contact us.<br>
-    Thank you for choosing our system</p>
-
+        <p>
+        Development credits: 
+            <ul style="margin-left: 20px;">
+            <li>Adam Smeaton</li>
+            <li>Asrin Dayananda</li>
+            <li>Mathew Norman</li>
+            <li>Michael Ferris</li>
+            <li>Yuan Ren</li>
+            <li>Jonathon Davis</li>
+            </ul>
+        </p>
+        
+        <p>If you experience any problems with this system, please contact us.<br>
+        Thank you for choosing our system</p>
+    </div>
 
 </div>
 </body>
