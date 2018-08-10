464dddac3cec206b192596e545738340d1618f90
Asrin Dayananda <adayanan@deakin.edu.au>
2018-08-10 16:40:16 +1000

    Update authenticate.php

--- Portal/php/reader/authenticate.php
+++ Portal/php/reader/authenticate.php
@@ -4,14 +4,14 @@
         $reader = $_GET['reader'];
         $key = $_GET['key'];
     if ($reader == 10 && $key == 9988) {
-        http_status_code(200);
+        http_response_code(200);
         echo "OK";
     } else {
-        http_status_code(403);
+        http_response_code(403);
         echo "Forbidden";
     }
 } else {
-    http_status_code(403);
+    http_response_code(403);
     echo "Forbidden";
 }
 ?>
