862a65a075e63033f1c3e0ada11174656e6a84f1
Asrin Dayananda <adayanan@deakin.edu.au>
2018-08-10 16:27:11 +1000

    Update authenticate.php

--- Portal/php/reader/authenticate.php
+++ Portal/php/reader/authenticate.php
@@ -1,3 +1,17 @@
 <?php
     // Readers will validate cards here.
+    if (isset($_GET['reader']) && isset($_GET['key'])) {
+        $reader = $_GET['reader'];
+        $key = $_GET['key'];
+    if ($reader == 10 && $key == 9988) {
+        http_status_code(200);
+        echo "OK";
+    } else {
+        http_status_code(403);
+        echo "Forbidden";
+    }
+} else {
+    http_status_code(403);
+    echo "Forbidden";
+}
 ?>
