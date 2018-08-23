011c711dfc860ab3c41a4dcfb2cf5644a948de59
Asrin Dayananda <adayanan@deakin.edu.au>
2018-08-23 20:47:29 +1000

    Update approve.php

--- Portal/php/reader/approve.php
+++ Portal/php/reader/approve.php
@@ -1,3 +1,23 @@
 <?php
     // Unverified readers will create an approval request here.
+    <?php
+
+    if (isset($_GET['id'])) {
+        $reader = $_GET['id'];
+
+        if (existReader($reader)) {
+            if(approvedReader($reader)){
+                echo 1;
+            }
+            else{
+                echo 0;
+            }
+
+        } else {
+            echo 0;
+        }
+    } else {
+        echo 0;
+    }
+    ?>
 ?>
