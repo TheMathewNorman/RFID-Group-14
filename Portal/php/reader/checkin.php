3187a28dbf8632a8eeb3f8c300c1e5c748ad571c
Mathew Norman <spam@matnorman.com>
2018-08-30 16:17:48 +1000

    Finalized reader checkin

--- Portal/php/reader/checkin.php
+++ Portal/php/reader/checkin.php
@@ -1,3 +1,15 @@
 <?php
-    // If a check-in system is set up, readers will log a check-in request here.
+    include_once('../database.php');
+    $database = new Database();
+
+    include_once('../validate.php');
+    $validate = new Validate();
+    
+    if (isset($_GET['key']) && isset($_GET['reader'])) {
+        $reader = $validate->sanitizeString($_GET['reader']);
+        $key = $validate->sanitizeString($_GET['key']);
+        if ($database->addLogEntry($readerid,$key, 1)) { 
+            die("1");
+        }
+    }
 ?>
