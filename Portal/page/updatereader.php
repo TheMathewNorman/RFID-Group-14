50b281d4173f3b8b2573c0f556d3fe4bc6e32467
Mathew Norman <spam@matnorman.com>
2018-09-02 06:16:51 +1000

    Debugging update reader approve

--- Portal/page/updatereader.php
+++ Portal/page/updatereader.php
@@ -19,14 +19,9 @@
         header("Location: ../php/deletereader.php?id=".$_GET['id']);
     }
     
-    if (isset($_POST['reader_name']) || isset($_POST['reader_group']) || isset($_POST['approved'])) {        
-        if ($_POST['approved']) {
-            $postApprove = 1;
-        } else {
-            $postApprove = 0;
-        }
-        
-        $database->updateReader($_GET['id'], $_POST['reader_name'], $_POST['reader_group'], $postApprove);
+    if (isset($_POST['reader_name']) || isset($_POST['reader_group']) || isset($_POST['approved'])) {
+        if ($_POST['approved'] != 1) { $_POST['approved'] = 0; }
+        $database->updateReader($_GET['id'], $_POST['reader_name'], $_POST['reader_group'], $_POST['approved']);
         header("Location: listreaders.php");
     } 
 ?>
@@ -47,9 +42,9 @@
                 <table class="form-table">
                 <tr><td style="text-align:right">Reader name: </td><td><input type="text" name="reader_name" placeholder="<?php echo $readerInfo['reader_name']; ?>"></td></tr>
                 <tr><td style="text-align:right">Reader group: </td><td><input type="number" name="reader_group" min="1" max="99" value="<?php echo $readerInfo['reader_group']; ?>"></td></tr>
-                <tr><td style="text-align:right">Approved:</td><td><input type="checkbox" name="approved" value="true" <?php echo $approved; ?>></td></tr>
+                <tr><td style="text-align:right">Approved:</td><td><input type="checkbox" name="approved" value="1" <?php echo $approved; ?>></td></tr>
                 <tr><td></td></tr>
-                <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="1"></td></tr>
+                <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="true"></td></tr>
                 <tr><td colspan="2" style="text-align:right"><input type="submit" value="Update Reader"> <input type="reset" value="Clear"></td></tr>
                 </table>
             </form>
