7cf7843b6adaee5b470584bef3bddab8e29bb58f
Mathew Norman <spam@matnorman.com>
2018-09-02 06:14:44 +1000

    debugging update reader

--- Portal/page/updatereader.php
+++ Portal/page/updatereader.php
@@ -19,10 +19,15 @@
         header("Location: ../php/deletereader.php?id=".$_GET['id']);
     }
     
-    if (isset($_POST['reader_name']) || isset($_POST['reader_group']) || isset($_POST['approved'])) {
-        echo $_POST['approved']."<br>";        
-        //$database->updateReader($_GET['id'], $_POST['reader_name'], $_POST['reader_group'], $_POST['approved']);
-        //header("Location: listreaders.php");
+    if (isset($_POST['reader_name']) || isset($_POST['reader_group']) || isset($_POST['approved'])) {        
+        if ($_POST['approved']) {
+            $postApprove = 1;
+        } else {
+            $postApprove = 0;
+        }
+        
+        $database->updateReader($_GET['id'], $_POST['reader_name'], $_POST['reader_group'], $postApprove);
+        header("Location: listreaders.php");
     } 
 ?>
 <html>
@@ -44,7 +49,7 @@
                 <tr><td style="text-align:right">Reader group: </td><td><input type="number" name="reader_group" min="1" max="99" value="<?php echo $readerInfo['reader_group']; ?>"></td></tr>
                 <tr><td style="text-align:right">Approved:</td><td><input type="checkbox" name="approved" value="true" <?php echo $approved; ?>></td></tr>
                 <tr><td></td></tr>
-                <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="true"></td></tr>
+                <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="1"></td></tr>
                 <tr><td colspan="2" style="text-align:right"><input type="submit" value="Update Reader"> <input type="reset" value="Clear"></td></tr>
                 </table>
             </form>
