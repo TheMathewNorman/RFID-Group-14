bdac4889ab70ab4c7b5112a0fac36bb6ea2e96ce
Asrin Dayananda <adayanan@deakin.edu.au>
2018-09-01 21:53:23 +1000

    Update updatereader.php

--- Portal/page/updatereader.php
+++ Portal/page/updatereader.php
@@ -1,1 +1,32 @@
-//update reader
+<?php 
+  if (!isset($GET'id')) {
+    header("location: listreaders.php");
+  }
+
+  include_once "../php/database.php
+  $database = new Database();
+
+?>
+
+<html>
+    <head>
+        <title>Update Member</title>
+        <link rel="stylesheet" type="text/css" href="../css/global-style.css">
+        <link rel="stylesheet" type="text/css" href="../css/header.css">
+        <link rel="stylesheet" type="text/css" href="../css/menu.css">
+    </head>
+    <body bgcolor="#f5f5f5">
+        <div id="content">
+        
+        <?php include "../component/header.php"; ?>
+        <?php include "../component/menu.php"; ?>
+            
+            <form action="" method="POST">
+                <table class="form-table">
+     
+                  
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
