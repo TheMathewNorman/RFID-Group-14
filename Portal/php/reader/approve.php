9738c67de099fdcf77c95af2c65f0fabb196e753
Mathew Norman <spam@matnorman.com>
2018-09-02 04:28:26 +1000

    Sanitize ID and reject empty IDs

--- Portal/php/reader/approve.php
+++ Portal/php/reader/approve.php
@@ -1,14 +1,19 @@
 <?php
     include_once('../database.php');
+    include_once('../validate.php');
     $database = new Database();
+    $validate = new Validate();
 
     $return = '0';
 
     if (isset($_GET['reader'])) {
         $readerid = $_GET['reader'];
-        // Check if reader is approved
-        if ($database->checkReaderApproved($readerid)) {
-            $return = '1';
+
+        if ($validate->sanitizeString($readerid) != "") {
+            // Check if reader is approved
+            if ($database->checkReaderApproved($readerid)) {
+                $return = '1';
+            }
         }
     }
 
