71c15d602e27ed599306a2f88e9b80b1800b12d9
Mathew Norman <spam@matnorman.com>
2018-09-02 05:56:49 +1000

    Adjusting update reader layout

--- Portal/page/updatereader.php
+++ Portal/page/updatereader.php
@@ -40,9 +40,9 @@
             <form action="" method="POST">
                 <table class="form-table">
                 <tr><td style="text-align:right">Reader name: </td><td><input type="text" name="reader_name" placeholder="<?php echo $readerInfo['reader_name']; ?>"></td></tr>
-                <tr><td style="text-align:right">Reader group: </td><td><input type="number" name="reader_group" placeholder="<?php echo $readerInfo['reader_group']; ?>"></td></tr>
+                <tr><td style="text-align:right">Reader group: </td><td><input type="number" name="reader_group" min="1" max="99" placeholder="<?php echo $readerInfo['reader_group']; ?>"></td></tr>
                 <tr><td style="text-align:right">Approved:</td><td><input type="checkbox" name="approved" value="true" <?php echo $approved; ?>></td></tr>
-                <tr></tr>
+                <tr><td></td></tr>
                 <tr><td style="text-align:right">Delete:</td><td><input type="checkbox" name="delete" value="true"></td></tr>
                 <tr><td colspan="2" style="text-align:right"><input type="submit" value="Update Member"> <input type="reset" value="Clear"></td></tr>
                 </table>
