8e14ba6877a1ddab71be2e3dfc2c3e30d11fef44
Mathew Norman <spam@matnorman.com>
2018-09-08 10:51:09 +1000

    Testing DataTables bare install

--- Portal/page/viewlogs.php
+++ Portal/page/viewlogs.php
@@ -25,7 +25,7 @@
 <html>
   <head>
     <title>Logs</title>
-    <link rel="stylesheet" type="text/css" href="../css/global-style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="../css/global-style.css"> -->
     <link rel="stylesheet" type="text/css" href="../css/header.css">
     <link rel="stylesheet" type="text/css" href="../css/menu.css">
 
